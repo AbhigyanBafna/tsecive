783af44258ec096c562bcb51a291f9959d4c81d5
MANAV  JAIN <manavjaingithub@gmail.com>
2023-10-19 11:50:21 +0530

    ekdfwikrg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,16 @@ import SearchToggle from '@/components/SearchToggle';
 
 const Navbar = ({ showGrid, setShowGrid }) => {
   return (
-    <nav className="bg-blue-500 p-4 px-10 text-white shadow-md">
+    <nav className="bg-white p-4 px-10 text-black shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-        <Image className="h-14 w-auto" width={500} height={500} src="/TSECive.png" alt='tsecive'/>
+        <Image className="h-28 w-auto" width={500} height={500} src="/TSECive.png" alt='tsecive'/>
 
         {showGrid !== undefined && (
           <SearchToggle showGrid={showGrid} setShowGrid={setShowGrid} />
         )}
 
         <div className="space-x-4">
-          <button className="bg-blue-700 hover:bg-blue-800 py-2 px-4 rounded">Login</button>
+          <button className="bg-blue-700 text-white hover:bg-blue-800 py-2 px-4 rounded">Login</button>
         </div>
       </div>
     </nav>
